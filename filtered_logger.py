2ef6caea6d5b5bd3b3621d9d76c7346aaca93c79
Orion <Orion45.dd@gmail.com>
2024-09-11 13:19:31 -0700

    Log formatter

--- filtered_logger.py
+++ filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Filtering log"""
 import re
+import logging
 
 
 def filter_datum(fields, redaction, message, separator):
@@ -8,3 +9,24 @@ def filter_datum(fields, redaction, message, separator):
     Obfuscation is the process of disguising confidential or sensitive data."""
     a = r"(" + '|'.join(fields) + r")=([^" + separator + "]*)"
     return re.sub(a, r"\1=" + redaction, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ The format method uses filter_datum to obfuscate the message and
+        returns the result"""
+        r = record.getMessage()
+        s = self.fields
+        record.msg = filter_datum(s, self.REDACTION, r, self.SEPARATOR)
+        return super().format(record)
