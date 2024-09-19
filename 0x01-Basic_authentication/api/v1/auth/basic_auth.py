8aaf27f2bbcf00e5cac3901457bd735a409fc4e8
Orion <Orion45.dd@gmail.com>
2024-09-19 13:02:41 +0000

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """BasicAuth"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -16,7 +17,21 @@ class BasicAuth(Auth):
                 return None
             if not authorization_header.startswith("Basic "):
                 return None
-            if 0 < len("Basic "):
-                return authorization_header
+            return authorization_header[len("Basic "):]
+        else:
+            return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """decode_base64_authorization_header Gives a valid Base64 string,
+        the method will decode it and return a string."""
+        if base64_authorization_header is not None:
+            if type(base64_authorization_header) != str:
+                return None
+            try:
+                data = base64.b64decode(base64_authorization_header)
+                return data.decode('utf-8')
+            except (base64.binascii.Error, UnicodeDecodeError):
+                return None
         else:
             return None
