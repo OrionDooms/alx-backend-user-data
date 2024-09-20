c83ebb52f02d66ea9c763e84f4a554161dd0b674
Orion <Orion45.dd@gmail.com>
2024-09-20 23:22:27 +0000

    User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""SessionAuth class that update authentication"""
 import uuid
 from api.v1.auth.auth import Auth
 from models.user import User
@@ -9,8 +10,22 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        if user_id is not None and type(user_id) == str:
-            client_id = str(uuid.uuid4())
-            SessionAuth.user_id_by_session_id[client_id] = user_id
-            return client_id
+        """create_session allows multiple session IDs to be associated
+        with the same user_id."""
+        if user_id is not None:
+            if type(user_id) == str:
+                client_id = str(uuid.uuid4())
+                self.user_id_by_session_id[client_id] = user_id
+                return client_id
+            return None
+        return None
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """user_id_for_session_id method fetch the user_id based on the
+        session ID and ensuring the authentication works by linking a user
+        to a session ID."""
+        if session_id is not None:
+            if type(session_id) == str:
+                return self.user_id_by_session_id.get(session_id)
+            return None
         return None
