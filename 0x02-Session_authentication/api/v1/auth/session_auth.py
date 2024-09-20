6ffbe9d7d499d0e2370591c11fad4ce15189c505
Orion <Orion45.dd@gmail.com>
2024-09-20 17:19:48 +0000

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """SessionAuth that inherits Auth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is not None and type(user_id) == str:
+            client_id = str(uuid.uuid4())
+            SessionAuth.user_id_by_session_id[client_id] = user_id
+            return client_id
+        return None
