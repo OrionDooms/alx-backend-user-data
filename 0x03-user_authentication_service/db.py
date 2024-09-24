65d4de3d4c6134efd4c3350e5b30be2f6cb93200
Orion <Orion45.dd@gmail.com>
2024-09-24 20:57:29 +0000

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,8 +3,10 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound as ORMNoResultFound
 
 from user import User, Base
 
@@ -38,3 +40,14 @@ class DB:
         session.add(client)
         session.commit()
         return client
+
+    def find_user_by(self, **kwargs) -> User:
+        """find_user_by it will print the user's ID or it will print an
+        error message based on the exception raised."""
+        session = self._session
+        user = session.query(User).filter_by(**kwargs).first()
+        if not kwargs:
+            raise InvalidRequestError("Invalid Request")
+        if user is None:
+            raise NoResultFound("NO Result Found")
+        return user
