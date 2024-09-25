1d118dfb453317a068de1b8fb4e2155cdea5bcde
Orion <Orion45.dd@gmail.com>
2024-09-25 15:22:14 +0000

    Hash password

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,9 +4,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import NoResultFound, InvalidRequestError
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session, scoped_session
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound
 
 from user import User, Base
 
