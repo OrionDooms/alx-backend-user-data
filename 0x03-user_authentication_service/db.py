d6bd4836c4d9dba3a5bd932d9b788548ef978d74
Orion <Orion45.dd@gmail.com>
2024-09-25 13:37:45 +0000

    update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,9 +4,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import NoResultFound, InvalidRequestError
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound as ORMNoResultFound
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import User, Base
 
@@ -60,3 +60,16 @@ class DB:
 
         except InvalidRequestError:
             raise InvalidRequestError("Invalid Request")
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """update_user updates, applies, changes and save to the database"""
+
+        client = self.find_user_by(id=user_id)
+
+        for key, value in kwargs.items():
+            if getattr(client, key, None) is None and not hasattr(client, key):
+                raise ValueError()
+            setattr(client, key, value)
+
+        self._session.commit()
+        return None
